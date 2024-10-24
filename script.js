0aa1c644b1b2e10c0be722a500e7e39c04c8aa50
SaraToth <saratoth101@gmail.com>
2024-10-24 17:09:41 +0900

    Create basic recursive tree function logic that returns the root object

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function createNode(data) {
         rightChild: null,
     }
 }
+
+function tree(array) {
+    //recursive stuff
+
+    const middle = Math.floor(array.length / 2);
+    const root = createNode(array[middle]);
+
+    //root.leftChild = tree(left)
+    //root.rightChild = tree(right);
+}
+
+function buildTree(array) {
+    //takes the tree and builds it
+}
