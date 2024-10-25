8fbb4002acb7e8d35c27cb17f56962615b61e197
SaraToth <saratoth101@gmail.com>
2024-10-25 15:31:17 +0900

    Create mergeSort function and call it within buildTree function to ensure that all arrays converted to Binary Search Trees are sorted first

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function tree(array, start = 0, end = (array.length - 1)) {
     const middle = Math.floor((end - start) / 2);
     const root = createNode(array[middle]);
 
-    //base case
+    //base case: When length becomes 1 children should remain "null"
     if(array.length > 1 ) {
         root.leftChild = tree(array.slice(0, (middle)));
         root.rightChild = tree(array.slice(middle + 1));
@@ -20,12 +20,52 @@ function tree(array, start = 0, end = (array.length - 1)) {
 }
 
 function buildTree(array) {
-    //takes the tree and builds it
+    const sortedArray = mergeSort(array);
 }
+
+
+function mergeSort(array) {
+    if(array.length === 1) {
+        return array;
+    }
+    const middle = Math.floor(array.length / 2);
+
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [],
+        leftIndex = 0,
+        rightIndex = 0;
+
+    while(leftIndex < left.length && rightIndex < right.length) {
+        if(left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+
+
+
+
+
 
 
 const evenArray = [1, 2, 3, 4, 5, 6];
 const oddArray = [1, 2, 3, 4, 5, 6, 7];
+const mixedArray = [5, 2, 3, 7, 4, 1, 6];
 
 const four = [1, 2, 3, 4];
 const seven = [1, 2, 3, 4, 5, 6, 7];
